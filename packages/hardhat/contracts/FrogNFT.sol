d067fb9e4cb382d5fba3b17b344e1dc36f3f9a04
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-04-23 15:40:03 +0200

    feat: add token and nft contract

---
+++ packages/hardhat/contracts/FrogNFT.sol
@@ -0,0 +1,101 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.3;
+
+// import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+import "./GGToken.sol";
+
+interface IReputyApp {
+	function addRating(
+		address user,
+		uint256 delta,
+		string memory action
+	) external;
+
+	function subRating(
+		address user,
+		uint256 delta,
+		string memory action
+	) external;
+}
+
+contract FrogNFT is ERC721URIStorage {
+	uint256 public constant BRONZE_THRESHOLD = 50 ether;
+	uint256 public constant SILVER_THRESHOLD = 300 ether;
+	uint256 public constant GOLD_THRESHOLD = 500 ether;
+	uint256 public constant NFT_AMOUNT = 9;
+	uint256 public constant CLAIM_NFT_REWARD = 20;
+
+	address public owner;
+	address public ggToken;
+	IReputyApp public reputyApp;
+	mapping(address => mapping(uint256 => uint256)) ownerCounter;
+
+	string private baseURI;
+
+	using Counters for Counters.Counter;
+	Counters.Counter private _tokenIds;
+
+	constructor(
+		address ggToken_,
+		string memory baseURI_
+	) ERC721("Frog NFT", "FNFT") {
+		baseURI = baseURI_;
+		ggToken = ggToken_;
+	}
+
+	function setReputyApp(address app) external {
+		reputyApp = IReputyApp(app);
+	}
+
+	function claimNFT(uint256 nftIndex) external {
+		require(GGToken(ggToken).burner() == address(this), "NO_ACCESS");
+		require(nftIndex >= 1 && nftIndex <= NFT_AMOUNT, "INVALID_NFT_INDEX");
+
+		uint256 tokens = BRONZE_THRESHOLD;
+		if (nftIndex > 7) {
+			tokens = GOLD_THRESHOLD;
+		} else if (nftIndex > 2) {
+			tokens = SILVER_THRESHOLD;
+		}
+		require(
+			GGToken(ggToken).balanceOf(msg.sender) >= tokens,
+			"INSUFFICIENT_FUNDS"
+		);
+
+		GGToken(ggToken).burnTokens(msg.sender, tokens);
+		_tokenIds.increment();
+		uint256 newItemId = _tokenIds.current();
+		_mint(msg.sender, newItemId);
+		_setTokenURI(newItemId, Strings.toString(nftIndex));
+		ownerCounter[msg.sender][nftIndex]++;
+
+		reputyApp.addRating(msg.sender, CLAIM_NFT_REWARD, "");
+	}
+
+	function getOwnershipCount(
+		address user
+	) external view returns (uint256[] memory) {
+		uint256[] memory count = new uint256[](NFT_AMOUNT + 1);
+		for (uint256 i = 1; i <= NFT_AMOUNT; i++) {
+			count[i] = ownerCounter[user][i];
+		}
+		return count;
+	}
+
+	modifier onlyOwner() {
+		require(msg.sender == owner, "OWNER_ONLY");
+		_;
+	}
+
+	function _baseURI() internal view override returns (string memory) {
+		return baseURI;
+	}
+
+	function setBaseURI(string memory baseURI_) external onlyOwner {
+		baseURI = baseURI_;
+	}
+}
---
+++ packages/hardhat/contracts/GGToken.sol
@@ -0,0 +1,89 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+// Useful for debugging. Remove when deploying to a live network.
+// import "hardhat/console.sol";
+
+// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
+// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+library Leaderboard {
+	struct User {
+		address addr;
+		uint256 balance;
+	}
+}
+
+/**
+ * A smart contract that allows changing a state variable of the contract and tracking the changes
+ * It also allows the owner to withdraw the Ether in the contract
+ * @author BuidlGuidl
+ */
+contract GGToken is ERC20 {
+	address public owner;
+	address public burner;
+	mapping(address => bool) existingUsers;
+	address[] public holders;
+
+	constructor(uint256 initialSupply) ERC20("Game Guild Token", "GG") {
+		owner = msg.sender;
+		_mint(msg.sender, initialSupply);
+	}
+
+	modifier onlyOwner() {
+		require(msg.sender == owner, "OWNER_ONLY");
+		_;
+	}
+
+	modifier onlyBurner() {
+		require(msg.sender == burner, "BURNER_ONLY");
+		_;
+	}
+
+	function getUsers() external view returns (Leaderboard.User[] memory) {
+		Leaderboard.User[] memory users = new Leaderboard.User[](
+			holders.length
+		);
+
+		for (uint256 i = 0; i < holders.length; i++) {
+			users[i].addr = holders[i];
+			users[i].balance = balanceOf(holders[i]);
+		}
+
+		return users;
+	}
+
+	function claimTokens(uint256 tokens) external {
+		_mint(msg.sender, tokens);
+		if (!existingUsers[msg.sender]) {
+			holders.push(msg.sender);
+		}
+		existingUsers[msg.sender] = true;
+	}
+
+	function burnTokens(address target, uint256 tokens) external onlyBurner {
+		_burn(target, tokens);
+	}
+
+	function rewardTokens(
+		address[] memory users,
+		uint256[] memory tokens
+	) external onlyOwner {
+		for (uint256 i = 0; i < users.length; i++) {
+			_mint(users[i], tokens[i]);
+			if (!existingUsers[users[i]]) {
+				holders.push(users[i]);
+			}
+			existingUsers[users[i]] = true;
+		}
+	}
+
+	function _beforeTokenTransfer(
+		address from,
+		address to,
+		uint256 amount
+	) internal override {
+		super._beforeTokenTransfer(from, to, amount);
+	}
+}
