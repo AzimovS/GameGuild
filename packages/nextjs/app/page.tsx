cc83c7e2286e37bf9d9b3976fb3a2fae30851fbe
Sherkhan Azimov <sherkhanazim@gmail.com>
2024-04-28 10:38:23 +0200

    fix: main for not connected wallets

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -43,75 +43,29 @@ const Home: NextPage = () => {
 
   return (
     <>
-      {synced != null && ownershipCount != null ? (
-        <div className="flex items-center flex-col flex-grow pt-10">
-          <div>
-            <div className="flex items-center mb-3">
-              <img alt="icon" src={rewardIcon.src} />
-              <div className="ml-4">
-                <p className="text-3xl font-bold text-white">Rewards</p>
-                <p className="text-lg text-white opacity-80">Play games and claim exclusive NFTs!</p>
+      <div className="flex items-center flex-col flex-grow pt-10">
+        <div>
+          <div className="flex items-center mb-3">
+            <img alt="icon" src={rewardIcon.src} />
+            <div className="ml-4">
+              <p className="text-3xl font-bold text-white">Rewards</p>
+              <p className="text-lg text-white opacity-80">Play games and claim exclusive NFTs!</p>
+            </div>
+          </div>
+
+          <div className="flex">
+            {!!synced && (
+              <div className="px-4 bg-blue-950 rounded-xl">
+                <p className="text-sm text-gray-300">Synced on-chain</p>
+                <p className="text-xl font-bold text-white">{formatEther(synced)}</p>
               </div>
-            </div>
+            )}
+          </div>
 
-            <div className="flex">
-              {!!synced && (
-                <div className="px-4 bg-blue-950 rounded-xl">
-                  <p className="text-sm text-gray-300">Synced on-chain</p>
-                  <p className="text-xl font-bold text-white">{formatEther(synced)}</p>
-                </div>
-              )}
-            </div>
-
-            <div className="animate-smooth-appear">
-              <p className="inline-block px-4 py-2 mt-8 text-xl text-white rounded-md bg-blue-950">Regular</p>
-              <div className="mt-4 grid grid-cols-12 gap-4">
-                {bronzeNFT.map((n, i) => (
-                  <div key={n.image} className="col-span-6">
-                    <NFTCard
-                      imageUrl={n.image}
-                      name={n.name}
-                      desc={n.requiredScore}
-                      contentLeft={n.gen}
-                      contentRight={n.supply}
-                      contentMain={n.description}
-                      handleClick={() => claimNFT(i + 1)}
-                      insufficient={Number(formatEther(synced)) < BRONZE_THRESHOLD}
-                      owned={!!ownershipCount?.[i + 1]}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            <div className="animate-smooth-appear">
-              <p className="inline-block px-4 py-2 mt-8 text-xl text-white rounded-md bg-gradient-to-r from-[#6b778b] to-[#abb0b9]">
-                Silver
-              </p>
-              <div className="mt-4 grid grid-cols-12 gap-x-4 gap-y-8 animate-smooth-appear">
-                {silverNFT.map((n, i) => (
-                  <div key={n.image} className="col-span-6">
-                    <NFTCard
-                      imageUrl={n.image}
-                      name={n.name}
-                      desc={n.requiredScore}
-                      contentLeft={n.gen}
-                      contentRight={n.supply}
-                      contentMain={n.description}
-                      handleClick={() => claimNFT(i + 3)}
-                      insufficient={Number(formatEther(synced)) < SILVER_THRESHOLD}
-                      owned={!!ownershipCount?.[i + 3]}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            <p className="inline-block px-4 py-2 mt-8 text-xl text-white rounded-md bg-gradient-to-r from-[#ffb600] to-[#eed08a]">
-              Gold
-            </p>
-            <div className="mt-4 grid grid-cols-12 gap-4 animate-smooth-appear">
-              {goldNFT.map((n, i) => (
+          <div className="animate-smooth-appear">
+            <p className="inline-block px-4 py-2 mt-8 text-xl text-white rounded-md bg-blue-950">Regular</p>
+            <div className="mt-4 grid grid-cols-12 gap-4">
+              {bronzeNFT.map((n, i) => (
                 <div key={n.image} className="col-span-6">
                   <NFTCard
                     imageUrl={n.image}
@@ -120,20 +74,60 @@ const Home: NextPage = () => {
                     contentLeft={n.gen}
                     contentRight={n.supply}
                     contentMain={n.description}
-                    handleClick={() => claimNFT(i + 8)}
-                    insufficient={Number(formatEther(synced)) < GOLD_THRESHOLD}
-                    owned={!!ownershipCount?.[i + 8]}
+                    handleClick={() => claimNFT(i + 1)}
+                    insufficient={synced == null ? true : Number(formatEther(synced)) < BRONZE_THRESHOLD}
+                    owned={!!ownershipCount?.[i + 1]}
                   />
                 </div>
               ))}
             </div>
           </div>
+
+          <div className="animate-smooth-appear">
+            <p className="inline-block px-4 py-2 mt-8 text-xl text-white rounded-md bg-gradient-to-r from-[#6b778b] to-[#abb0b9]">
+              Silver
+            </p>
+            <div className="mt-4 grid grid-cols-12 gap-x-4 gap-y-8 animate-smooth-appear">
+              {silverNFT.map((n, i) => (
+                <div key={n.image} className="col-span-6">
+                  <NFTCard
+                    imageUrl={n.image}
+                    name={n.name}
+                    desc={n.requiredScore}
+                    contentLeft={n.gen}
+                    contentRight={n.supply}
+                    contentMain={n.description}
+                    handleClick={() => claimNFT(i + 3)}
+                    insufficient={synced == null ? true : Number(formatEther(synced)) < SILVER_THRESHOLD}
+                    owned={!!ownershipCount?.[i + 3]}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <p className="inline-block px-4 py-2 mt-8 text-xl text-white rounded-md bg-gradient-to-r from-[#ffb600] to-[#eed08a]">
+            Gold
+          </p>
+          <div className="mt-4 grid grid-cols-12 gap-4 animate-smooth-appear">
+            {goldNFT.map((n, i) => (
+              <div key={n.image} className="col-span-6">
+                <NFTCard
+                  imageUrl={n.image}
+                  name={n.name}
+                  desc={n.requiredScore}
+                  contentLeft={n.gen}
+                  contentRight={n.supply}
+                  contentMain={n.description}
+                  handleClick={() => claimNFT(i + 8)}
+                  insufficient={synced == null ? true : Number(formatEther(synced)) < GOLD_THRESHOLD}
+                  owned={!!ownershipCount?.[i + 8]}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      ) : (
-        <div className="mt-14 text-center">
-          <span className="loading loading-spinner loading-lg"></span>
-        </div>
-      )}
+      </div>
     </>
   );
 };
